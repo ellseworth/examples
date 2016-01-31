1e5e857a46b606b411754e65c16b0cce3c4b0014
anton urban <gdemoigusi@gmail.com>
2016-01-31 15:12:06 +0400

    изменение системы отслежываемых параметров по результатам их использования: вместо вложенных интерфейсов - абстрактные классы, вместо общего класса-контейнера - фабрика по созданию различных параметров

---
+++ ParameterFabric.cs
@@ -0,0 +1,173 @@
+﻿using System;
+
+namespace Common
+{
+	/// <summary>
+	/// статичная фабрика, создает реализации параметров
+	/// <seealso cref="Parameter<Towner, Tvalue>"/> и <seealso cref="ChangableParameterBase<Towner, Tvalue>"/>
+	/// эти реализации скрыты, и отличаются только способами сравнения значений
+	/// </summary>
+	public class Parameter
+	{
+		#region definition
+		//реализует вариант ParameterBase для работы с ссылочными типами данных
+		private class ForClass<Town, Tval> : Parameter<Town, Tval> where Tval : class
+		{
+			public ForClass(Town owner, out Action<Tval> change, out Action dispose, Action<Tval, Tval> preChange, Tval val) :
+				base(owner, out change, out dispose, preChange, val) { }
+
+			//выдает true, если ссылки ссылаются на один объект
+			protected override bool IsEquals(Tval first, Tval second) { return object.ReferenceEquals(first, second); }
+		}
+		//реализует вариант ParameterBase для работы с типами значений,
+		//для предотвращения упаковки при сравнении такие типы должны реализовать IEquatable<T>
+		private class ForStruct<Town, Tval> : Parameter<Town, Tval> where Tval : struct, IEquatable<Tval>
+		{
+			public ForStruct(Town owner, out Action<Tval> change, out Action dispose, Action<Tval, Tval> preChange, Tval val) :
+				base(owner, out change, out dispose, preChange, val) { }
+
+			//сравнивает два значения по определенному в них методу Equals
+			protected override bool IsEquals(Tval first, Tval second) { return ((IEquatable<Tval>)first).Equals(second); }
+		}
+		//реализует вариант ParameterBase для работы в енумами
+		private class ForEnum<Town, Tval> : Parameter<Town, Tval> where Tval : struct, IComparable, IFormattable, IConvertible
+		{
+			public ForEnum(Town owner, out Action<Tval> change, out Action dispose, Action<Tval, Tval> preChange, Tval val) :
+				base(owner, out change, out dispose, preChange, val) { }
+			
+			protected override bool IsEquals(Tval first, Tval second) { return first.GetHashCode() == second.GetHashCode(); }
+		}
+		//аналогичен ParameterForClass, только расширяет ChangableParameterBase
+		private class ChangebleForClass<Town, Tval> : ChangableParameter<Town, Tval> where Tval : class
+		{
+			public ChangebleForClass(Town owner, out Action dispose, Action<Tval, Tval> preChange, Tval val) :
+				base(owner, out dispose, preChange, val) { }
+
+			//выдает true, если ссылки ссылаются на один объект
+			protected override bool IsEquals(Tval first, Tval second) { return object.ReferenceEquals(first, second); }
+		}
+		//аналогичен ParameterForStruct, только расширяет ChangableParameterBase
+		private class ChangebleForStruct<Town, Tval> : ChangableParameter<Town, Tval> where Tval : struct, IEquatable<Tval>
+		{
+			public ChangebleForStruct(Town owner, out Action dispose, Action<Tval, Tval> preChange, Tval val) :
+				base(owner, out dispose, preChange, val) { }
+
+			//сравнивает два значения по определенному в них методу Equals
+			protected override bool IsEquals(Tval first, Tval second) { return ((IEquatable<Tval>)first).Equals(second); }
+		}
+		//аналогичен ParameterForEnum, только расширяет ChangableParameterBase
+		private class ChangableForEnum<Town, Tval> : ChangableParameter<Town, Tval> where Tval : struct, IComparable, IFormattable, IConvertible
+		{
+			public ChangableForEnum(Town owner, out Action dispose, Action<Tval, Tval> preChange, Tval val) :
+				base(owner, out dispose, preChange, val) { }
+
+			protected override bool IsEquals(Tval first, Tval second) { return first.GetHashCode() == second.GetHashCode(); }
+		}
+		#endregion
+
+		/// <summary>
+		/// создает новый параметр <see cref="Parameter<Towner, Tvalue>"/>, хранящий значения ссылочного <paramref name="Tvalue"/> типа 
+		/// </summary>
+		/// <typeparam name="Towner">тип владельца параметра</typeparam>
+		/// <typeparam name="Tvalue">тип хранимого значения, должен быть ссылочным</typeparam>
+		/// <param name="owner">хранитель и, скорее всего, создатель параметра</param>
+		/// <param name="changeAction">указатель на метод, меняющий в параметре значение</param>
+		/// <param name="disposeAction">указатель на метод,выгружающий параметр</param>
+		/// <param name="preChangeAction">действие, выполняемое перед сменой значения</param>
+		/// <param name="defaultValue">первоначальное значение</param>
+		/// <returns>не null-значение</returns>
+		public static Parameter<Towner, Tvalue> CreateForClass<Towner, Tvalue>(
+			Towner owner, out Action<Tvalue> changeAction, out Action disposeAction,
+			Action<Tvalue, Tvalue> preChangeAction = null, Tvalue defaultValue = default(Tvalue)
+			) where Tvalue : class
+		{
+			return new ForClass<Towner, Tvalue>(owner, out changeAction, out disposeAction, preChangeAction, defaultValue);
+		}
+		/// <summary>
+		/// создает новый параметр <see cref="Parameter<Towner, Tvalue>"/>, хранящий значения типа значения <paramref name="Tvalue"/> 
+		/// </summary>
+		/// <typeparam name="Towner">тип владельца параметра</typeparam>
+		/// <typeparam name="Tvalue">тип хранимого значения, должен быть типом значения</typeparam>
+		/// <param name="owner">хранитель и, скорее всего, создатель параметра</param>
+		/// <param name="changeAction">указатель на метод, меняющий в параметре значение</param>
+		/// <param name="disposeAction">указатель на метод, выгружающий параметр</param>
+		/// <param name="preChangeAction">действие, выполняемое перед сменой значения</param>
+		/// <param name="defaultValue">первоначальное значение</param>
+		/// <returns>не null-значение</returns>
+		public static Parameter<Towner, Tvalue> CreateForStruct<Towner, Tvalue>(
+			Towner owner, out Action<Tvalue> changeAction, out Action disposeAction,
+			Action<Tvalue, Tvalue> preChangeAction = null, Tvalue defaultValue = default(Tvalue)
+			) where Tvalue : struct, IEquatable<Tvalue>
+		{
+			return new ForStruct<Towner, Tvalue>(owner, out changeAction, out disposeAction, preChangeAction, defaultValue);
+		}
+		/// <summary>
+		/// создает новый параметр <see cref="Parameter<Towner, Tvalue>"/>, хранящий значения enum-типа <paramref name="Tvalue"/> 
+		/// </summary>
+		/// <typeparam name="Towner">тип владельца параметра</typeparam>
+		/// <typeparam name="Tvalue">тип хранимого значения, его базовым типом должен быть <seealso cref="Enum"/></typeparam>
+		/// <param name="owner">хранитель и, скорее всего, создатель параметра</param>
+		/// <param name="changeAction">указатель на метод, меняющий в параметре значение</param>
+		/// <param name="disposeAction">указатель на метод,выгружающий параметр</param>
+		/// <param name="preChangeAction">действие, выполняемое перед сменой значения</param>
+		/// <param name="defaultValue">первоначальное значение</param>
+		/// <returns>не null-значение</returns>
+		public static Parameter<Towner, Tvalue> CreateForEnum<Towner, Tvalue>(
+			Towner owner, out Action<Tvalue> changeAction, out Action disposeAction,
+			Action<Tvalue, Tvalue> preChangeAction = null, Tvalue defaultValue = default(Tvalue)
+			) where Tvalue : struct, IComparable, IFormattable, IConvertible
+		{
+			if(typeof(Tvalue).BaseType != typeof(Enum))
+				throw new ArgumentException(string.Format("Тип {0} не наследует Enum (не является enum)", typeof(Tvalue).FullName));
+			return new ForEnum<Towner, Tvalue>(owner, out changeAction, out disposeAction, preChangeAction, defaultValue);
+		}
+		/// <summary>
+		/// создает новый параметр <see cref="ChangableParameter<Towner, Tvalue>"/>, хранящий значения ссылочного <paramref name="Tvalue"/> типа 
+		/// </summary>
+		/// <typeparam name="Towner">тип владельца параметра</typeparam>
+		/// <typeparam name="Tvalue">тип хранимого значения, должен быть ссылочным</typeparam>
+		/// <param name="owner">хранитель и, скорее всего, создатель параметра</param>
+		/// <param name="disposeAction">указатель на метод,выгружающий параметр</param>
+		/// <param name="preChangeAction">действие, выполняемое перед сменой значения</param>
+		/// <param name="defaultValue">первоначальное значение</param>
+		/// <returns>не null-значение</returns>
+		public static ChangableParameter<Towner, Tvalue> CreateChangableForClass<Towner, Tvalue>(
+			Towner owner, out Action disposeAction, Action<Tvalue, Tvalue> preChangeAction = null, Tvalue defaultValue = default(Tvalue)
+			) where Tvalue : class
+		{
+			return new ChangebleForClass<Towner, Tvalue>(owner, out disposeAction, preChangeAction, defaultValue);
+		}
+		/// <summary>
+		/// создает новый параметр <see cref="ChangableParameter<Towner, Tvalue>"/>, хранящий значения типа значения <paramref name="Tvalue"/> 
+		/// </summary>
+		/// <typeparam name="Towner">тип владельца параметра</typeparam>
+		/// <typeparam name="Tvalue">тип хранимого значения, должен быть типом значения</typeparam>
+		/// <param name="owner">хранитель и, скорее всего, создатель параметра</param>
+		/// <param name="disposeAction">указатель на метод, выгружающий параметр</param>
+		/// <param name="preChangeAction">действие, выполняемое перед сменой значения</param>
+		/// <param name="defaultValue">первоначальное значение</param>
+		/// <returns>не null-значение</returns>
+		public static ChangableParameter<Towner, Tvalue> CreateChangableForStruct<Towner, Tvalue>(
+			Towner owner, out Action disposeAction, Action<Tvalue, Tvalue> preChangeAction, Tvalue defaultValue = default(Tvalue)
+			) where Tvalue : struct, IEquatable<Tvalue>
+		{
+			return new ChangebleForStruct<Towner, Tvalue>(owner, out disposeAction, preChangeAction, defaultValue);
+		}
+		/// <summary>
+		/// создает новый параметр <see cref="ChangableParameter<Towner, Tvalue>"/>, хранящий значения enum-типа <paramref name="Tvalue"/> 
+		/// </summary>
+		/// <typeparam name="Towner">тип владельца параметра</typeparam>
+		/// <typeparam name="Tvalue">тип хранимого значения, его базовым типом должен быть <seealso cref="Enum"/></typeparam>
+		/// <param name="owner">хранитель и, скорее всего, создатель параметра</param>
+		/// <param name="disposeAction">указатель на метод,выгружающий параметр</param>
+		/// <param name="preChangeAction">действие, выполняемое перед сменой значения</param>
+		/// <param name="defaultValue">первоначальное значение</param>
+		/// <returns>не null-значение</returns>
+		public static ChangableParameter<Towner, Tvalue> CreateChangableForEnum<Towner, Tvalue>(
+			Towner owner, out Action disposeAction, Action<Tvalue, Tvalue> preChangeAction, Tvalue defaultValue = default(Tvalue)
+			) where Tvalue : struct, IComparable, IFormattable, IConvertible
+		{
+			return new ChangableForEnum<Towner, Tvalue>(owner, out disposeAction, preChangeAction, defaultValue);
+		}
+	}
+}
---
+++ ParametersBase.cs
@@ -0,0 +1,151 @@
+﻿using System;
+
+namespace Common
+{
+	/// <summary>
+	/// параметр, представляющий значение и сообщение его изменении, как единый объект.
+	/// объект данного типа не является самостоятельным классом, его должен содержать объект-владелец параметра.
+	/// получение значения из данного параметра сводится к его неявному привидению к типу хранимого значения.
+	/// про получении нового значения, оно принимается, если не эквивалентно старому (классы сравниваются по ссылкам, структуры - по значениям)
+	/// после принятия нового значения вызывается событие <see cref="Changed"/>
+	/// </summary>
+	/// <typeparam name="Towner">тип владельца параметра</typeparam>
+	/// <typeparam name="Tvalue">тип хранимого значения</typeparam>
+	public abstract class Parameter<Towner, Tvalue>
+	{
+		/// <summary>
+		/// возвращает хранимое параметром значение
+		/// </summary>
+		/// <param name="source">приводимый параметр</param>
+		/// <returns>текущее значение параметра</returns>
+		public static implicit operator Tvalue(Parameter<Towner, Tvalue> source) { return source._value; }
+
+		//владелец параметра, значение закрыто для доступа,
+		//так как это противоречит принцыпу использования параметра,
+		//как значения какого-либо объекта-владельца параметра
+		private Towner _owner;
+		//действие, выполняемое перед тем, как изменить значение <старое значение, новое значение>
+		private Action<Tvalue, Tvalue> _preChange;
+
+		private Tvalue _value;
+		/// <summary>
+		/// возвращает или задает значение параметра, если оно изменяется, то вызывается событие <see cref="Changed"/>
+		/// </summary>
+		protected Tvalue ProtValue
+		{
+			get { return _value; }
+			set
+			{
+				//проверяем эквивалентность объектов перегруженным методом сравнения
+				//если значения одинаковы, ничего не делаем
+				if (IsEquals(_value, value)) return;
+				//сначала выполняем действия, предусмотренные перед сменой значения
+				if (_preChange != null) _preChange(_value, value);
+				//запоминаем старое значение, в некоторых случаях это важно
+				Tvalue from = _value;
+				//запоминаем новое значение
+				_value = value;
+				//извещаем об изменении
+				if (_changed != null) _changed(_owner, from, value);
+			}
+		}
+
+		//флаг выгрузки параметра
+		private bool _isDisposed;
+		//выгрузка ресурсов параметра, сами действия выполняются один раз, вне зависимости от количетсва вызовов этого метода
+		private void Dispose()
+		{
+			//предохранитель вызова
+			if (_isDisposed) return;
+			_isDisposed = true;
+
+			//обнуляем все делегаты и устанавливаем дефолтные значения полей
+			_changed = null;
+			_preChange = null;
+			_value = default(Tvalue);
+			_owner = default(Towner);
+		}
+
+		//делегат извещения изменения события, явная реализация нужна, чтобы предотвратить двойную подписку
+		private Action<Towner, Tvalue, Tvalue> _changed;
+		/// <summary>
+		/// вызывается, после смены значения в параметре <владелец параметра(для удобства), старое значение, новое значение>
+		/// предотвращает двойную подписку одного и того же делегата
+		/// </summary>
+		public event Action<Towner, Tvalue, Tvalue> Changed
+		{
+			add { _changed -= value; _changed += value; }
+			remove { _changed -= value; }
+		}
+
+		/// <summary>
+		/// создает параметр, в соответствии с моделью использования данног класса параметр должен создавать его владелец
+		/// он же при создании получит возможности управлять этим параметром (установка нового значения, выгрузка параметра)
+		/// </summary>
+		/// <param name="owner">владелей параметра</param>
+		/// <param name="preChange">действие, выполняемое перед сменой значения параметра</param>
+		/// <param name="value">первоначально значение параметра, устанавливается , как начальное значение без вызова <see cref="Changed"/></param>
+		/// <param name="changeAction">указатель на действие смены значения <новое значение>,
+		/// оно должно остатья у владельца параметра после создания</param>
+		/// <param name="disposeAction">указатель на действие, выполняющее выгрузку параметра,
+		/// оно должно остаться у владельца параметра после создания</param>
+		protected Parameter(
+			Towner owner, out Action<Tvalue> changeAction, out Action disposeAction,
+			Action<Tvalue, Tvalue> preChange = null, Tvalue value = default(Tvalue)
+			) : this(owner, out disposeAction, preChange, value)
+		{
+			changeAction = val => ProtValue = val;
+		}
+
+		/// <summary>
+		/// создает параметр, в соответствии с моделью использования данного класса параметр должен создавать его владелец
+		/// он же при создании получит возможности управлять этим параметром (установка нового значения, выгрузка параметра)
+		/// данный конструктор нужен только дл удобства создания наследников, имеющих возможность устанавливать значения параметра публично
+		/// </summary>
+		/// <param name="owner">владелец параметра</param>
+		/// <param name="preChange">действие, выполняемое перед сменой значения параметра</param>
+		/// <param name="value">первоначально значение параметра, устанавливается , как начальное значение без вызова <see cref="Changed"/></param>
+		/// <param name="disposeAction">указатель на действие, выполняющее выгрузку параметра,
+		/// оно должно остаться у владельца параметра после создания</param>
+		protected Parameter(
+			Towner owner, out Action disposeAction, Action<Tvalue, Tvalue> preChange = null, Tvalue value = default(Tvalue)
+			)
+		{
+			_owner = owner;
+			_value = value;
+			_preChange = preChange;
+			disposeAction = () => Dispose();
+		}
+
+		//определяет метод сравнения старого и нового значений
+		protected abstract bool IsEquals(Tvalue first, Tvalue second);
+	}
+	/// <summary>
+	/// параметр, представляющий значение и сообщение его изменении, как единый объект.
+	/// данный класс дополняет функционал базового <seealso cref="Parameter<Towner, Tvalue>"/>
+	/// возможностью изменять значение через свойсотво <see cref="Value"/>
+	/// </summary>
+	/// <typeparam name="Towner">тип владельца параметра</typeparam>
+	/// <typeparam name="Tvalue">тип хранимого значения</typeparam>
+	public abstract class ChangableParameter<Towner, Tvalue> : Parameter<Towner, Tvalue>
+	{
+		/// <summary>
+		/// открытое свойство для установки нового значения
+		/// </summary>
+		public Tvalue Value { set { ProtValue = value; } }
+
+		/// <summary>
+		/// создает параметр, значение которого публично доступно для установки,
+		/// в соответствии с моделью использования данного класса параметр должен создавать его владелец
+		/// он же при создании получит возможности управлять этим параметром (выгрузка параметра)
+		/// </summary>
+		/// <param name="owner">владелец параметра</param>
+		/// <param name="disposeAction">указатель на действие, выполняющее выгрузку параметра,
+		/// оно должно остаться у владельца параметра после создания</param>
+		/// <param name="preChange">действие, выполняемое перед сменой значения параметра</param>
+		/// <param name="value">первоначальное значение параметра, устанавливается, как начальное значение без вызова <see cref="Changed"/></param>
+		protected ChangableParameter(
+			Towner owner, out Action disposeAction, Action<Tvalue, Tvalue> preChange = null, Tvalue value = default(Tvalue) 
+			) : base(owner, out disposeAction, preChange, value) { }
+	}
+}
